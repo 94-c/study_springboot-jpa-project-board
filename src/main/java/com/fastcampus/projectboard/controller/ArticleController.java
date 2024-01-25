6f59119a71377e4cf4aedca62b2ef6ec42901997
94-c <hyeongwoo26@gmail.com>
2024-01-25 12:55:32 +0900

    #33 - [refactoring] 게시판 뷰 기능 구현 - dto

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -84,7 +84,7 @@ public class ArticleController {
     public String postNewArticle(ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "boom", "asdf1234", "boom@mail.com", "boom", "memo", null, null, null, null
+                "boom", "asdf1234", "boom@mail.com", "boom", "memo"
         )));
 
         return "redirect:/articles";
@@ -104,7 +104,7 @@ public class ArticleController {
     public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "boom", "asdf1234", "boom@mail.com", "boom", "memo", null, null, null, null
+                "boom", "asdf1234", "boom@mail.com", "boom", "memo"
         )));
 
         return "redirect:/articles/" + articleId;
