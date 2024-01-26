1965e9fd126b1958637b46e62a837410abfab1ac
HyeongWooChoi <hw.choi@catenoid.net>
2024-01-26 15:44:11 +0900

    #24 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 수정

--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
@@ -3,30 +3,17 @@ package com.fastcampus.projectboard.dto.response;
 import com.fastcampus.projectboard.dto.ArticleCommentDto;
 
 import java.time.LocalDateTime;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id,
         String content,
         LocalDateTime createdAt,
         String email,
-        String nickname,
-        String userId,
-        Long parentCommentId,
-        Set<ArticleCommentResponse> childComments
+        String nickname
 ) {
 
-    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
-        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
-    }
-
-    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
-        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
-                .comparing(ArticleCommentResponse::createdAt)
-                .thenComparingLong(ArticleCommentResponse::id);
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname) {
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname);
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -35,18 +22,13 @@ public record ArticleCommentResponse(
             nickname = dto.userAccountDto().userId();
         }
 
-        return ArticleCommentResponse.of(
+        return new ArticleCommentResponse(
                 dto.id(),
                 dto.content(),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
-                nickname,
-                dto.userAccountDto().userId()
+                nickname
         );
-    }
-
-    public boolean hasParentComment() {
-        return parentCommentId != null;
     }
 
 }
--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -1,14 +1,10 @@
 package com.fastcampus.projectboard.dto.response;
 
-import com.fastcampus.projectboard.dto.ArticleCommentDto;
 import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
 
 import java.time.LocalDateTime;
-import java.util.Comparator;
-import java.util.Map;
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
@@ -19,12 +15,11 @@ public record ArticleWithCommentsResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        String userId,
         Set<ArticleCommentResponse> articleCommentsResponse
 ) {
 
-    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtags, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentResponse> articleCommentResponses) {
-        return new ArticleWithCommentsResponse(id, title, content, hashtags, createdAt, email, nickname, userId, articleCommentResponses);
+    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
     }
 
     public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
@@ -41,33 +36,10 @@ public record ArticleWithCommentsResponse(
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
-                dto.userAccountDto().userId(),
-                organizeChildComments(dto.articleCommentDtos())
+                dto.articleCommentDtos().stream()
+                        .map(ArticleCommentResponse::from)
+                        .collect(Collectors.toCollection(LinkedHashSet::new))
         );
     }
-
-    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
-        Map<Long, ArticleCommentResponse> map = dtos.stream()
-                .map(ArticleCommentResponse::from)
-                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
-
-        map.values().stream()
-                .filter(ArticleCommentResponse::hasParentComment)
-                .forEach(comment -> {
-                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
-                    parentComment.childComments().add(comment);
-                });
-
-        return map.values().stream()
-                .filter(comment -> !comment.hasParentComment())
-                .collect(Collectors.toCollection(() ->
-                        new TreeSet<>(Comparator
-                                .comparing(ArticleCommentResponse::createdAt)
-                                .reversed()
-                                .thenComparingLong(ArticleCommentResponse::id)
-                        )
-                ));
-    }
-
 
 }
