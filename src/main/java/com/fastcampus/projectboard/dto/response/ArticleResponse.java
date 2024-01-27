fb32965b9ee66631f431433946377fd539488fd2
94-c <hyeongwoo26@gmail.com>
2024-01-27 20:07:01 +0900

    #25 - 수정, 삭제를 할 때 해당 사용자가 맞는지 확인하기 위한 응답 정보 추가

--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleResponse.java
@@ -8,14 +8,14 @@ public record ArticleResponse(
         Long id,
         String title,
         String content,
-       String hashtag,
+        String hashtag,
         LocalDateTime createdAt,
         String email,
         String nickname
 ) {
 
-    public static ArticleResponse of(Long id, String title, String content, String hashtags, LocalDateTime createdAt, String email, String nickname) {
-        return new ArticleResponse(id, title, content, hashtags, createdAt, email, nickname);
+    public static ArticleResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname) {
+        return new ArticleResponse(id, title, content, hashtag, createdAt, email, nickname);
     }
 
     public static ArticleResponse from(ArticleDto dto) {
