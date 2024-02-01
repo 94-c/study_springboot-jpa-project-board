ff81b13a311ada365c41ad4927d5fea5baad6e79
94-c <hyeongwoo26@gmail.com>
2024-02-01 18:15:43 +0900

    #50 - 게시글 뷰 수정

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -19,7 +19,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
 
 @RequiredArgsConstructor
 @RequestMapping("/articles")
