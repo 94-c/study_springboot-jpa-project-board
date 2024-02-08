41d826974b1880ac4556176321ebcc4a5cfa2067
HyeongWooChoi <hw.choi@catenoid.net>
2024-02-08 16:00:50 +0900

    #91 - Security 생성한 로그인 폼 적용

--- src/main/java/com/project/board/global/config/SecurityConfig.java
+++ src/main/java/com/project/board/global/config/SecurityConfig.java
@@ -49,7 +49,12 @@ public class SecurityConfig {
                         ).hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/login-form") // 사용자 정의 로그인 페이지 경로 지정
+                        .permitAll()
+                        //.defaultSuccessUrl("/dashboard", true) // 로그인 성공 후 이동할 경로
+                        //.failureUrl("/login?error=true") // 로그인 실패 시 이동할 경로
+                )
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .oauth2Login(oAuth -> oAuth
                         .userInfoEndpoint(userInfo -> userInfo
